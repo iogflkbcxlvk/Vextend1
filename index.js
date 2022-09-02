de29f9b5fec236566a19e6918a6760965d45e6dc
iogflkbcxlvk <103741658+iogflkbcxlvk@users.noreply.github.com>
2022-09-03 00:32:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const tokenfile = require("./tokenfile.json");
+const botconfig = require("./botconfig.json");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async() =>{
+    console.log(`${bot.user.username} Elindult!`)
+
+    let státuszok = [
+        "VextendNetwork",   
+        "Eskü nem vagyok robot!😋",
+    ]
+
+
+    setInterval(function(){
+        let status = státuszok[Math.floor(Math.random()* státuszok.length)]
+
+        bot.user.setActivity(status, {type:"PLAYING"})
+    }, 3000)
+})
+
+bot.login(tokenfile.token);
