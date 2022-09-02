215305557b6c4ee0d9bc2e732118353b98b45014
iogflkbcxlvk <103741658+iogflkbcxlvk@users.noreply.github.com>
2022-09-03 00:33:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const tokenfile = require("./tokenfile.json");
 const botconfig = require("./botconfig.json");
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -19,4 +18,4 @@ bot.on("ready", async() =>{
     }, 3000)
 })
 
-bot.login(tokenfile.token);
+bot.login(process.env.BOT_TOKEN);
